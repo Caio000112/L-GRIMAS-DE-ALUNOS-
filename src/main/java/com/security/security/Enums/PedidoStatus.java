a7cd4298a9abe19c6516359607d92548b2b951d6
Caio000112 <caiobrunotoledo@gmail.com>
2025-10-06 20:31:01 -0300

    atualizado

--- src/main/java/com/security/security/Enums/PedidoStatus.java
+++ src/main/java/com/security/security/Enums/PedidoStatus.java
@@ -1,4 +1,7 @@
 package com.security.security.Enums;
 
 public enum PedidoStatus {
+    AGUARDANDO_PAGAMENTO,
+    EM_ANDAMENTO,
+    PRONTO,
 }
--- src/main/java/com/security/security/Model/Pedido.java
+++ src/main/java/com/security/security/Model/Pedido.java
@@ -1,8 +1,8 @@
 package com.security.security.Model;
 
+import com.security.security.Enums.PedidoStatus;
 import jakarta.persistence.*;
 
-@Table(name = "tbl_order")
 @Entity
 public class Pedido {
 
@@ -10,8 +10,46 @@ public class Pedido {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    private Double preco;
+
+    private PedidoStatus pedidoStatus;
 
 
+    public Pedido (){
+
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public PedidoStatus getPedidoStatus() {
+        return pedidoStatus;
+    }
+
+    public void setPedidoStatus(PedidoStatus pedidoStatus) {
+        this.pedidoStatus = pedidoStatus;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
 
 
